6e0cbe69fb9ed3d5ef8cfa110e773e7b9c64edaa
Diego Oliveira <diego.fernandes@gmail.com>
2023-05-04 15:16:22 -0300

    finalizando o curso

--- js/validacao.js
+++ js/validacao.js
@@ -2,12 +2,29 @@ function ChutePossuiValorValido(chute) {
     const numero = +chute;
 
     if(chuteForInvalido(numero)) {
-        console.log('Valor inválido')
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return;
     }
 
     if(numeroPermitido(numero)) {
-        console.log(`Valor inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}`);
+        elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor} </div>`;
+        return;
     }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if(numero > numeroSecreto) {
+        elementoChute.innerHTML += ` <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
+
+    } else {
+        elementoChute.innerHTML += ` <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
+    }
+
 }
 
 function chuteForInvalido(numero) {
@@ -17,3 +34,9 @@ function chuteForInvalido(numero) {
 function numeroPermitido(numero) {
     return numero > maiorValor || numero < menorValor;
 } 
+
+document.body.addEventListener('click', (e) => {
+    if(e.target.id === 'jogar-novamente') {
+        window.location.reload();
+    }
+})
